5fb444161d7f4b7e2b320054295769c7b5cfb702
Jeffrey Sabo <saboj2@tcnj.edu>
2018-11-20 12:13:22 -0500

    11/20 end of class

--- finalproject.py
+++ finalproject.py
@@ -5,38 +5,47 @@ import scipy.io.wavfile
 import matplotlib.pyplot as plt
 
 #scale down audio file?
-fs, d_original = scipy.io.wavfile.read("champions.wav", False)
+fs, d_original = scipy.io.wavfile.read("championsShort.wav", False)
 
-A = 10000
+d_original = d_original[1000000:1010000]
+
+A = 100
 t = numpy.arange(len(d_original))
-noise =  A*numpy.sin(60*t)
+
+
+d_original = d_original/32768.0
+
+
+noise =  A*numpy.sin(60*t/1000)
 d_noise = d_original + noise
 
 y = numpy.zeros(len(d_original))
 e = numpy.zeros(len(d_original))
 
-filter_order = 10
-w = numpy.ones(filter_order)
-lr = 0.1
+filter_order = 100
+w = numpy.zeros(filter_order)
+lr = 0.00000001
 
-noise = numpy.pad(noise,(filter_order,filter_order),'constant', constant_values=(0,0))
-print(len(t))
+noise = 5*numpy.pad(noise,(filter_order-1,filter_order-1),'constant', constant_values=(0,0))
+
+
+
 for n in t:
-    print(float(n)/len(t) * 100)
-    for m in range(0, filter_order-1):
-        y[n] += noise[n-m]*w[m]
+
+    for m in range(0, filter_order):
+        #fix for negative indexing
+        y[n] += noise[n-m+filter_order-1]*w[m]
 
     e[n] = d_noise[n] - y[n]
-    w = w + lr*noise[n]*e[n]
 
-d_filtered = d_noise-y
+    for j in range(0, filter_order):
+        w[j] = w[j] + lr*noise[n-j]*e[n]
 
 plt.figure(1)
 plt.plot(t, d_original)
-
 plt.figure(2)
 plt.plot(t, d_noise)
 plt.figure(3)
-plt.plot(t, d_filtered)
+plt.plot(e)
 
 plt.show()
