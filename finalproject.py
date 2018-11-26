e024febb103798849ff45642ed93d40c77773025
Jeffrey Sabo <saboj2@tcnj.edu>
2018-11-26 17:23:46 -0500

    11/26 part 2

--- finalproject.py
+++ finalproject.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 #scale down audio file?
 fs, d_original = scipy.io.wavfile.read("champions.wav", False)
 
-d_original = d_original[10000:1010000]
+d_original = d_original[1000000:1100000]
 
 A = 1
 t = numpy.arange(len(d_original))
@@ -19,7 +19,12 @@ d_original = d_original/32768.0
 noise =  A*numpy.sin(60*t/1000.0)
 d_noise = d_original + noise
 
-scipy.io.wavfile.write("champions_noise.wav", fs, d_noise)
+plt.figure(1)
+plt.plot(d_original)
+
+plt.figure(2)
+plt.plot(d_noise)
+
 
 y = numpy.zeros(len(d_original))
 e = numpy.zeros(len(d_original))
@@ -43,15 +48,19 @@ for n in t:
     for j in range(0, filter_order):
         w[j] = w[j] + lr*noise[n-j+filter_order-1]*e[n]
 
-scipy.io.wavfile.write("champions_filter.wav", fs, e)
 
-plt.figure(1)
-plt.plot(d_original)
-plt.figure(2)
-plt.plot(d_noise)
 plt.figure(3)
 plt.plot(e)
+#plt.ylim((-0.1, 0.1))
+
 plt.figure(4)
 plt.plot(y)
 
 plt.show()
+
+
+d_noise = numpy.asarray(d_noise*32768).astype(numpy.int16)
+scipy.io.wavfile.write("champions_noise.wav", fs, d_noise)
+
+e = numpy.asarray(e*32768).astype(numpy.int16)
+scipy.io.wavfile.write("champions_filter.wav", fs, e)
